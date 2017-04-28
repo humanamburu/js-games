6eb2de636ba4b9564fe983ea12679ab3ce7fe97e
humanamburu <uladzimir_halushka@epam.com>
2017-04-28 15:05:45 +0300

    [Reborn] Configure server stub

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+const staticPath = path.resolve('server/static/');
+const appStatic = express.static(staticPath);
+
+app.use(appStatic);
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s). [REACT-ROUTER]
+app.get('*', (request, response) => {
+    response.sendFile(`${staticPath}'/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Server was started on port ${port}`);
+});
