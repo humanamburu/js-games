a7969b03e968c9590f42a509ac9c76a7e29415d6
humanamburu <humanamburu@ojooo.com>
2017-04-30 01:00:22 +0300

    Add main-wrapper component

---
+++ client/src/components/main-wrapper/buttons.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    key: 'main',
+    title: 'Main Page',
+    icon: 'appstore',
+    href: '#'
+  },
+  {
+    key: 'profile',
+    title: 'Profile',
+    icon: 'meh-o',
+    href: '#'
+  },
+  {
+    key: 'game',
+    title: 'Game',
+    icon: 'code',
+    href: '#'
+  },
+  {
+    key: 'results',
+    title: 'Results',
+    icon: 'pie-chart',
+    href: '#'
+  },
+  {
+    key: 'materials',
+    title: 'Materials',
+    icon: 'book',
+    href: '#'
+  },
+]
--- client/src/components/welcome/welcome.jsx
+++ client/src/components/welcome/welcome.jsx
@@ -1,65 +1,17 @@
 import './_welcome.styl';
 
-import { Component } from 'react';
+import React from "react";
 import { connect } from 'react-redux';
-import { Layout, Icon } from 'antd';
 
-import SideBar from 'components/side-bar/side-bar';
+import MainWrapper from 'components/main-wrapper/main';
 
-const { Header, Content } = Layout;
-
-class Welcome extends Component {
-    constructor(props) {
-        super(props);
-    }
-
+class Welcome extends React.Component {
     render() {
         const { username } = this.props;
-        const sideBarButtons = [
-            {
-                key: 'main',
-                title: 'Main Page',
-                icon: 'appstore',
-                href: '#'
-            },
-            {
-                key: 'profile',
-                title: 'Profile',
-                icon: 'meh-o',
-                href: '#'
-            },
-            {
-                key: 'game',
-                title: 'Game',
-                icon: 'code',
-                href: '#'
-            },
-            {
-                key: 'results',
-                title: 'Results',
-                icon: 'pie-chart',
-                href: '#'
-            },
-            {
-                key: 'materials',
-                title: 'Materials',
-                icon: 'book',
-                href: '#'
-            },
-        ];
-
         return (
-            <Layout className="container">
-                <SideBar buttons={sideBarButtons} selected={'main'}/>
-                <Layout>
-                    <Header/>
-                    <Content style={{ margin: '20px 16px' }}>
-                        <div style={{ padding: 24, background: '#fff', minHeight: '100%' }}>
-                            Bill is a cat.
-                        </div>
-                    </Content>
-                </Layout>
-            </Layout>
+            <MainWrapper>
+                <div>Hello world</div>
+            </MainWrapper>
         );
     }
 }
