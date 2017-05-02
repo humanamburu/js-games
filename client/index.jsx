60bc67e7b6304d7ca5431e58e2853719ad2424c8
humanamburu <humanamburu@ojooo.com>
2017-05-03 00:55:03 +0300

    Move UI code to src

--- client/index.jsx
+++ client/index.jsx
@@ -1,40 +1,9 @@
-import './_default.scss';
-
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux'
 
-import Welcome from 'components/welcome/welcome';
-import MainWrapper from 'components/main-wrapper/main-wrapper';
-import Game from 'components/game/game';
-import Materials from 'components/materials/materials';
-import Results from 'components/results/results';
-import Profile from 'components/profile/profile';
-import NotFound from 'components/404/404';
-
-import store from 'store/store';
-import history from 'store/history';
-
+import UI from 'index';
 
 //Needed for React Developer Tools
 window.React = React;
 
-render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <MainWrapper>
-                <Switch>
-                    <Route exact path="/" component={Welcome}/>
-                    <Route path="/game" component={Game}/>
-                    <Route path="/profile" component={Profile}/>
-                    <Route path="/results" component={Results}/>
-                    <Route path="/materials" component={Materials}/>
-                    <Route component={NotFound}/>
-                </Switch>
-            </MainWrapper>
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('react-root')
-);
+render(<UI/>, document.getElementById('react-root'));
---
+++ client/src/index.jsx
@@ -0,0 +1,34 @@
+import './_default.scss';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux'
+
+import Welcome from 'components/welcome/welcome';
+import MainWrapper from 'components/main-wrapper/main-wrapper';
+import Game from 'components/game/game';
+import Materials from 'components/materials/materials';
+import Results from 'components/results/results';
+import Profile from 'components/profile/profile';
+import NotFound from 'components/404/404';
+
+import store from 'store/store';
+import history from 'store/history';
+
+export default () => (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <MainWrapper>
+                <Switch>
+                    <Route exact path="/" component={Welcome}/>
+                    <Route path="/game" component={Game}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/results" component={Results}/>
+                    <Route path="/materials" component={Materials}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </MainWrapper>
+        </ConnectedRouter>
+    </Provider>
+);
